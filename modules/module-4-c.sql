f2e7931aceda1863928400677abfd665318e2ed4
Jegors Čemisovs <jegors_cemisovs@epam.com>
2024-06-26 17:55:35 +0300

    Add new SQL module for customer information
    A new SQL module, module-4-c.sql, has been created to retrieve common customer information. The query returns the first name, last name, and email of all customers, ordered by their first name in ascending order and last name in descending order.

---
+++ modules/module-4-c.sql
@@ -0,0 +1,12 @@
+/*
+  Common customer information
+ */
+SELECT
+    FirstName AS [Customer First Name],
+    LastName  AS 'Customer Last Name',
+    Email AS EMAIL
+FROM
+    Customer
+ORDER BY
+    FirstName ASC,
+    LastName DESC
